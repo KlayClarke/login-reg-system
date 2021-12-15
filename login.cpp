46c1f7c64acc200200ebeeccc57e02bf965cad95
Klay Clarke <klayaclarke@gmail.com>
2021-12-15 17:25:11 -0500

    give user options of login or registration. If user chooses registration, allow them to create username and password / create new .txt file in working directory with users username and password

--- login.cpp
+++ login.cpp
@@ -4,53 +4,86 @@
 
 using namespace std;
 
-bool IsLoggedIn() 
-{
-    string entered_username, entered_password, correct_username, correct_password;
+// bool IsLoggedIn() 
+// {
+//     string entered_username, entered_password, correct_username, correct_password;
 
-    correct_username = "correct_username";
-    correct_password = "correct_password";
+//     cout << "Username: ";
+//     cin >> entered_username;
+//     cout << "Password: ";
+//     cin >> entered_password;
 
-    cout << "Username: ";
-    cin >> entered_username;
-    cout << "Password: ";
-    cin >> entered_password;
+//     ifstream read(entered_username + '.txt');
+//     getline(read, correct_username);
+//     getline(read, correct_password);
 
-    if (entered_username == correct_username && entered_password == correct_password)
-    {
-        cout << "Access Granted\n";
-        return true;
-    }
-    else
-    {
-        if (entered_username == correct_username && entered_password != correct_password)
-        {
-            cout << "Incorrect password\n\n";
-        }
+//     if (entered_username == correct_username && entered_password == correct_password)
+//     {
+//         cout << "Access Granted\n";
+//         return true;
+//     }
+//     else
+//     {
+//         if (entered_username == correct_username && entered_password != correct_password)
+//         {
+//             cout << "Incorrect password\n\n";
+//         }
 
-        else if (entered_username != correct_username && entered_password == correct_password)
-        {
-            cout << "Incorrect username\n\n";
-        }
+//         else if (entered_username != correct_username && entered_password == correct_password)
+//         {
+//             cout << "Incorrect username\n\n";
+//         }
 
-        else 
-        {
-            cout << "Incorrect username and password\n\n";
-        }
+//         else 
+//         {
+//             cout << "Incorrect username and password\n\n";
+//         }
 
-        return false;
-    }
+//         return false;
+//     }
 
-}
+// }
 
 int main()
 {
-    while (!IsLoggedIn())
+    int choice;
+
+    cout << "Press 1 to register, 2 to login: ";
+    cin >> choice;
+
+    if (choice == 1) // if user chooses to register
     {
-        IsLoggedIn();
-        cin.clear();
-        cin.ignore();
+        string username, password;
+
+        cout << "\nenter a username: ";
+        cin >> username;
+        cout << "\nenter a password: ";
+        cin >> password;
+
+        ofstream file (username + ".txt");
+        file << username << endl << password;
+        file.close();
+
+        main();
     }
 
+    else if (choice == 2) // if user chooses to login
+    {
+        // bool status = IsLoggedIn();
+
+        // if (!status)
+        // {
+        //     cout << "Login attempt unsuccessful!";
+        // }
+
+        // else 
+        // {
+        //     cout << "Login attempt successful!";
+        // }
+
+        cout << "Loading...";
+    }
+    
     return 0;
+
 }
