088fc3d330f30d2ee384b9c505936ccda3a74187
Klay Clarke <klayaclarke@gmail.com>
2021-12-15 15:29:53 -0500

    initialize variables, house correct username and password in respective variables (as string), take user input for entered_username and entered_password variables, if username input is same as correct values, display Access Granted message to user

--- login.cpp
+++ login.cpp
@@ -4,8 +4,29 @@
 
 using namespace std;
 
+bool IsLoggedIn() 
+{
+    string entered_username, entered_password, correct_username, correct_password;
+
+    correct_username = "correct_username";
+    correct_password = "correct_password";
+
+    cout << "Username: ";
+    cin >> entered_username;
+    cout << "Password: ";
+    cin >> entered_password;
+
+    if (entered_username == correct_username && entered_password == correct_password)
+    {
+        cout << "Access Granted";
+        return true;
+    }
+    
+}
+
 int main()
 {
+    IsLoggedIn();
     // Initialize variables for username and password - as username and password strings
 
     // Take user input for username
