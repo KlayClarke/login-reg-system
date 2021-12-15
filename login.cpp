ec26ca5b4099dc3e6703153b03098b2ec355df7c
Klay Clarke <klayaclarke@gmail.com>
2021-12-15 14:45:01 -0500

    init

---
+++ login.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // Initialize variables for username and password - as username and password strings
+
+    // Take user input for username
+
+    // Take user input for password
+
+    // If user input for username and password is equal to username and password strings, print "Access Granted"
+
+    // Else, print "Access Denied"
+}
