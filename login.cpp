a2d625479e318eda514ede16ad5db9bc8da8edf8
Klay Clarke <klayaclarke@gmail.com>
2021-12-15 19:00:50 -0500

    format

--- login.cpp
+++ login.cpp
@@ -3,8 +3,7 @@
 #include <string>
 using namespace std;
 
-bool IsLoggedIn() 
-{
+bool IsLoggedIn() {
     string entered_username, entered_password, correct_username, correct_password;
     // prompt user to enter username, password
     cout << "Username: ";
@@ -15,70 +14,79 @@ bool IsLoggedIn()
     getline(file, correct_username); // initialize correct_username variable as text on first line 
     getline(file, correct_password); // initialize correct_password variable as text on second line
     // if user's entered username and password are found in txt file - means user is a user within db
-    if (entered_username == correct_username && entered_password == correct_password)
-    {
+    if (entered_username == correct_username && entered_password == correct_password) {
         cout << "Access Granted\n";
         return true;
     }
-    else
-    {   // if user's entered username is found but entered password is not - means user may've forgotten password
+    else {   // if user's entered username is found but entered password is not - means user may've forgotten password
         if (entered_username == correct_username && entered_password != correct_password)
         {
             cout << "Incorrect password\n\n";
         }
         // if user's entered username is not found but entered password is - means user may've forgotten username
-        else if (entered_username != correct_username && entered_password == correct_password)
-        {
+        else if (entered_username != correct_username && entered_password == correct_password) {
             cout << "Incorrect username\n\n";
         }
-        else // if both entries are not found
-        {
+        // if both entries are not found
+        else {
             cout << "Incorrect username and password\n\n";
         }
         return false;
     }
 }
 
-int main()
-{
+int main() {
     int choice;
     cout << "Press 1 to register, 2 to login: "; // prompt user to either register or login - options
     cin >> choice; // take user choosing as choice variable
-    if (choice == 1) // if user chose to register
-    {
+    // if user chose to register
+    if (choice == 1) {
         string username, password;
-        
         cout << "\nenter a username: "; // prompt user to enter unique username
         cin >> username;
-
         ifstream file (username + ".txt"); //check to see if username exists in 'database'
-        if (file) // if username exists
-        {
+        // if username exists
+        if (file) {
             cout << "Account with this username already exists!\n";
         }
-        else // if not, allow user to continue registration
-        {
+        // if not, allow user to continue registration
+        else {
             cout << "\nenter a password: "; // prompt user to enter password
             cin >> password;
-
             ofstream file (username + ".txt"); //create txt file in working directory titled '{username}.txt'
             file << username << endl << password; //write user's username to one line and password to another line in txt file
             file.close(); //close text file
         }
         main();
     }
-    else if (choice == 2) // if user chose to login
-    {
-        bool status = IsLoggedIn(); // IsLoggedIn is returning either true or false - depends on whether user passed correct values through
-        if (!status) //if status / IsLoggedIn() is false
-        {
+    // if user chose to login
+    else if (choice == 2) {
+        bool status = IsLoggedIn(); // run IsLoggedIn and return either true or false - depends on whether user passed correct values through
+        //if IsLoggedIn() is false
+        if (!status) {
             cout << "Login attempt unsuccessful!";
         }
-        else  //if status / IsLoggedIn() is true
-        {
-            cout << "Login attempt successful!";
+        //if IsLoggedIn() is true
+        else {  
+            bool logout = false;
+            int choice;
+            cout << "Press 1 to access secrets, 2 to logout: ";
+            while (!logout) {
+                cin >> choice;
+                if (choice == 1) {
+                    cout << "SECRETS...SECRETS...SECRETS...SECRETS...SECRETS";
+                    logout = false;
+                }
+                else if (choice == 2) {
+                    cout << "Have a nice day!";
+                    logout = true;
+                }
+            }
         }
     }
     return 0;
 }
+// after accessing secrets, allow user to choose between secrets or logout again
+// after logout, give user choice to register or login again
 // once user logged in, give option to logout - if user chooses to logout, give option to either register or login
+// option to change username -- ??? or change password -- ???
